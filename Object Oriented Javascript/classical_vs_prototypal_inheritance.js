07518675ce0f74fbe69ad0c159feb34c6eef2e9d
jsgilberto <alv.mtz94@gmail.com>
2018-06-03 17:58:29 +0200

    added classical vs prototypal inheritance

---
+++ Object Oriented Javascript/classical_vs_prototypal_inheritance.js
@@ -0,0 +1,18 @@
+/**
+ * Classical vs Prototypal Inheritance
+ * Inheritance: One object gets access to the
+ * properties and methods of another object.
+ * 
+ * Prototypal Inheritance:
+ *  Simple
+ *    flexible
+ *    extensible
+ *    easy to understand
+ * 
+ * Classical Inheritance:
+ *  Verbose
+ *    friend
+ *    protected
+ *    private
+ *    interface
+**/
